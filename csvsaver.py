7e71a046c8ecf186e93ef2843967af1a0b5b4fb8
maxsrobotics <66796793+maxsrobotics@users.noreply.github.com>
2021-08-20 18:53:21 -0400

    Update csvsaver.py

--- csvsaver.py
+++ csvsaver.py
@@ -1,1 +1,15 @@
+import csv
+import datetime as dt
 
+def sensor_write(S):
+  with open ('Readings/___.csv', 'a') as log:
+        log_writer = csv.writer(log)
+        log_writer.writerow({S})
+
+file = open("Readings/___.csv", "r+")
+file.truncate
+file.close
+
+start = dt.datetime.now()
+while (dt.datetime.now() - start).seconds < 10:
+    sensor_write(temp)
